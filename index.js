df12b6c2522d77e457e98164f6639d8035be5783
GusLight <90425648+GusLight@users.noreply.github.com>
2022-04-02 05:33:02 +0200

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,9 @@
+function Block(letter, rate, used, father, code) {
+  this.letter = letter;
+  this.rate = rate;
+  this.used = used;
+  this.father = father;
+  this.code = code;
+}
+
+function findCode(block) {
